b81f24d9805625d363a9a5811ddc807d1747c731
Souradeep321 <souradeephazra93@gmail.com>
2026-01-26 12:30:59 +0530

    feat: add admin authentication middleware for protected routes

---
+++ proxy.ts
@@ -0,0 +1,23 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function proxy(req: NextRequest) {
+    const token = await getToken({
+        req,
+        secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (req.nextUrl.pathname.startsWith("/admin")) {
+        if (!token || token.role !== "ADMIN") {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/admin/:path*"],
+};
